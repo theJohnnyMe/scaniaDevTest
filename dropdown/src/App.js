60b148db9d10fcfb8dc50ed36cf66b26f0a83143
thejohnnyme <johnny.bradata.kurva@gmail.com>
2021-06-30 17:33:20 +0200

    Adding app-container, header and subheader elements and styles

--- dropdown/src/App.js
+++ dropdown/src/App.js
@@ -2,8 +2,16 @@ import './styles/App.css';
 
 function App() {
   return (
-    <div className="App">
-        <h1>Hello world</h1>
+    <div className="app-container">
+        <header>
+            <p className="header-title">Fleet portal</p>
+            <div className="header-logo"/>
+        </header>
+        <div className="subheader">
+            <p className="subheader-title">
+                Driver evaluation
+            </p>
+        </div>
     </div>
   );
 }
