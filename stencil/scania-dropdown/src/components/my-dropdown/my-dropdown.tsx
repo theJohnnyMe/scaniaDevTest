fadc5085dcbca20ff810fccb90c624a52566a451
thejohnnyme <johnny.bradata.kurva@gmail.com>
2021-07-02 11:50:34 +0200

    Adding reset button

--- stencil/scania-dropdown/src/components/my-dropdown/my-dropdown.tsx
+++ stencil/scania-dropdown/src/components/my-dropdown/my-dropdown.tsx
@@ -22,30 +22,36 @@ export class MyDropdown {
   dropdownToggleCSSClass = () => "dropdown-placeholder " + (this.isOpened ? "dropdown-placeholder--opened" : "");
 
 
-  getDropdownValue = (event) => {
-    this.placeholderText = document.getElementById("label_" + event.target.id).textContent;
+  handleOnChange = (event) => {
+    this.placeholderText = event.target.value.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",") + " km";
     this.placeholderValue = event.target.value;
     this.isOpened = false;
   }
 
-  componentDidUpdate() {
-    console.log("New value is:" + document.getElementById("my-dropdown-placeholder").getAttribute("value"));
+  handleReset = () => {
+    this.placeholderValue = 0;
+    this.placeholderText = this.placeholder;
   }
 
+
   render() {
-    return <div>
-        <button id="my-dropdown-placeholder" class={this.dropdownToggleCSSClass()} onClick={this.toggleDropdown} value={this.placeholderValue}>
+
+    return <div class="dropdown-wrapper">
+      <div class="dropdown">
+        <button class={this.dropdownToggleCSSClass()} onClick={this.toggleDropdown} value={this.placeholderValue}>
           <span class="placeholder-text">{this.placeholderText}</span>
           <span class="placeholder-arrow">
             <svg width="12" height="7" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M1 1l5 5 5-5" stroke="#000" stroke-width="1.3" stroke-linecap="round" stroke-linejoin="round"/></svg>
           </span>
         </button>
         <div class="dropdown-options">
-          <input type="radio" id="option_1" name="distance" value="200000" data-text="<= 200 000 km" onChange={this.getDropdownValue} />
-          <label id="label_option_1" htmlFor="option_1">{"<= 200 000 km"}</label>
-          <input type="radio" id="option_2" name="distance" value="200001" data-text="> 200 000 km" onChange={this.getDropdownValue} />
-          <label id="label_option_2" htmlFor="option_2">{"> 200 000 km"}</label>
+          <input type="radio" id="option_1" name="distance" value="<= 200000" onChange={this.handleOnChange} />
+          <label htmlFor="option_1">{"<= 200 000 km"}</label>
+          <input type="radio" id="option_2" name="distance" value="> 200000" onChange={this.handleOnChange} />
+          <label htmlFor="option_2">{"> 200 000 km"}</label>
         </div>
       </div>
+      <button class="dropdown-reset" onClick={this.handleReset}>Reset</button>
+    </div>
   }
 }
