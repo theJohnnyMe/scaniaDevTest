56378887666a1c4e56b595b9b0fa67ce0ff69e0d
thejohnnyme <johnny.bradata.kurva@gmail.com>
2021-07-05 20:59:50 +0200

    Angular Success!

--- angular-app/src/main.ts
+++ angular-app/src/main.ts
@@ -4,7 +4,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
-import { defineCustomElements } from 'stencil-custom-dropdown/loader';
+import { defineCustomElements } from 'johnny-custom-dropdown/loader';
 
 if (environment.production) {
   enableProdMode();
