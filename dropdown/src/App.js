f8d64954211c1725b5463050fcdc117573667de7
thejohnnyme <johnny.bradata.kurva@gmail.com>
2021-07-05 14:19:11 +0200

    cleaning unused code, styles alignment

--- dropdown/src/App.js
+++ dropdown/src/App.js
@@ -1,66 +1,18 @@
-import './styles/App.css';
-
-const tableData = [
-    {
-        driver: "Marcus Lundberg",
-        company: "Aris FC",
-        distance: 75044,
-        score: 52,
-    },
-    {
-        driver: "Marcus Mena Pacheco",
-        company: "Lio LTD",
-        distance: 129417,
-        score: 95,
-    },
-    {
-        driver: "Valentine Ichtertz",
-        company: "LOTS Group",
-        distance: 244656,
-        score: 67,
-    },
-    {
-        driver: "Niklas Rosén",
-        company: "DD Interactive",
-        distance: 200000,
-        score: 78,
-    },
-]
+import "./styles/App.css";
 
 function App() {
   return (
     <div className="app-container">
-        <header>
-            <p className="header-title">Fleet portal</p>
-            <div className="header-logo"/>
-        </header>
-        <div className="subheader">
-            <p className="subheader-title">
-                Driver evaluation
-            </p>
-        </div>
-        <main>
-            <table>
-                <thead>
-                <tr>
-                    <th>Driver</th>
-                    <th>Company</th>
-                    <th>Distance</th>
-                    <th>Score</th>
-                </tr>
-                </thead>
-                <tbody>
-                {tableData.map((item) => (
-                    <tr key={item.id}>
-                        <td>{item.driver}</td>
-                        <td>{item.company}</td>
-                        <td>{item.distance} km</td>
-                        <td>{item.score}</td>
-                    </tr>
-                ))}
-                </tbody>
-            </table>
-        </main>
+      <header>
+        <p className="header-title">Fleet portal</p>
+        <div className="header-logo" />
+      </header>
+      <div className="subheader">
+        <p className="subheader-title">Driver evaluation</p>
+      </div>
+      <main>
+        <p>Stencil Component place</p>
+      </main>
     </div>
   );
 }
